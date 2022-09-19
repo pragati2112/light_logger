9918d09e46c7eaa114d8d1a154fc6f98fc5280d6
pragati2112 <pandeypragati2112@gmail.com>
2022-09-19 19:43:45 +0530

    code optimization

--- server/stream_logs.py
+++ server/stream_logs.py
@@ -4,20 +4,26 @@ import re
 import datetime
 
 log_file_path = '/var/log/dpkg.log'
+regex = r"([0-9]{4}-[0-9]{2}-[0-9]{2})"
+
+
+def find_match(item: str = ''):
+    match = re.search(regex, item)
+    return datetime.datetime.strptime(match.group(0), '%Y-%m-%d')
 
 
 def find(endless_haystack, needle):
-    if endless_haystack[0] == needle:
+    if find_match(endless_haystack[0]) == needle:
         return 0
 
     i = 1
-    hay = endless_haystack[i]
-    while hay < needle:  # this is O(log n) where n is the index of the element
+    while i <= len(endless_haystack) and find_match(endless_haystack[i]) <= needle:
+        # this is O(log n) where n is the index of the element
         i = 2 * i
-        hay = endless_haystack[i]
 
     # from the loop before the element is between i and i // 2
-    return bisect.bisect_left(endless_haystack, needle, i // 2, i)
+    # return bisect.bisect_left(endless_haystack, needle, i // 2, i)
+    return i//2, i
 
 
 async def read_logs_from_files(start_date: str = '2022-09-07', end_date: str = '2022-09-17',
@@ -25,12 +31,20 @@ async def read_logs_from_files(start_date: str = '2022-09-07', end_date: str = '
     # fetch latest logs 50---
     with open(log_file_path, "r") as f:
         logs = []
-        regex = r"([0-9]{4}-[0-9]{2}-[0-9]{2})"
 
-        for item in f.readlines():
+        endless_hay = f.readlines()
+
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+        start_idx, _ = find(endless_haystack=endless_hay, needle=start_date)
+        _, end_idx = find(endless_haystack=endless_hay, needle=end_date)
+
+        for item in endless_hay[start_idx: end_idx+1]:
             match = re.search(regex, item)
             if match is not None:
-                if datetime.datetime.strptime(start_date, '%Y-%m-%d') <= datetime.datetime.strptime(match.group(0),'%Y-%m-%d') <= datetime.datetime.strptime(end_date, '%Y-%m-%d'):
+                matched_date = datetime.datetime.strptime(match.group(0), '%Y-%m-%d')
+                if start_date <= matched_date <= end_date:
                     logs.append(item)
 
         if page_no == 1:
